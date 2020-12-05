af933d776a57983537c4a0233372e5f745dffaf9
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-12-05 20:35:30 +0530

    Changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,14 @@
 // write code for Welcome component here
 import React from "react";
-import '../styles/App.css';
-
+import "../styles/App.css";
 
 const Welcome = (props) => {
   return (
     <>
-      <h1> Hey {props.name}</h1>
-      <h2> Welcome to Newton School.</h2>
+      <h1> Hey ! {props.name}</h1>
+      <h2> "Welcome to Newton School."</h2>
     </>
-  )
-}
-
+  );
+};
 
 export default Welcome;
