77b86c4fa62859d1a740abfc9dd6c6fdb20a2a58
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-12-05 20:39:55 +0530

    changess

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -6,7 +6,7 @@ const Welcome = (props) => {
   return (
     <>
       <h1> Hey ! {props.name}</h1>
-      <h2> "Welcome to Newton School."</h2>
+      <h2> Welcome to Newton School</h2>
     </>
   );
 };
